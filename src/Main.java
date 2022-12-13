4d6c50b34f23af625c2e74c477529529baaa5c69
Linka <Polina153@mail.ru>
2022-12-13 20:45:10 +0400

    2 step: reading text files + images

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,103 @@ import java.util.List;
 
 public class Main {
     static private List<File> files;
+    static private List<File> textFiles;
+
 
     public static void main(String[] args) {
+        System.out.println("List of all files in directory 1: ");
         listOfFiles("1", files);
-        readFiles();
+        System.out.println();
+        System.out.println("Text files are: ");
+        //filterFiles("1", getFilteredFiles());
+        filteredFiles(files);
+        readFiles(getTextFiles());
+    }
+
+    static void filteredFiles(List<File> files) {
+        List<File> filteredFiles = new ArrayList<>();
+        for (File f : files) {
+            if (f.getName().endsWith(".txt")) {
+                System.out.println(f.getName());
+                filteredFiles.add(f);
+            }
+            setTextFiles(filteredFiles);
         }
 
-    private static void readFiles() {
+    }
+
+    static void filterFiles(String directoryName, List<File> filteredFiles) {
+        /*if (fileFromDir.getName().endsWith(".txt")) {
+            getTextFiles() textFiles.add(fileFromDir);
+            setTextFiles(textFiles);
+            *//*
+         *//**//* directory.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File file, String s) {
+                    return s.endsWith(".txt");
+                }
+            });*//**//*
+            List<File> textFiles = List.of(Objects.requireNonNull(directory.listFiles()));
+            setTextFiles(textFiles);*//*
+        }*/
+
+       /* File directory = new File(directoryName);
+        if (directory.exists()) {
+            File[] fList = directory.listFiles();
+            if (fList != null) {
+                for (File file : fList) {
+                    if (file.isFile()) {
+                        if (filteredFiles == null) {
+                            setFilteredFiles(new ArrayList<>());
+                            getFilteredFiles().add(file);
+                        } else {
+                            getFilteredFiles().add(file);
+                        }
+
+                    } else if (file.isDirectory()) {
+                        filterFiles(file.getAbsolutePath(), getFilteredFiles());
+                    }
+                }
+            }
+            assert fList != null;
+            for (File f : fList) {
+                if (f.getName().endsWith(".txt")) {
+                    getTextFiles().add(f);
+                    setTextFiles(textFiles);
+                }
+            }
+        } else {
+            System.out.println("Sorry, wrong name of directory!");
+        }*/
+    }
+
+    static void readFiles(List<File> listToRead) {
         String[] texts;
-        for (File file:files){
+        for (File file : listToRead) {
             System.out.println();
             System.out.println(file.getName());
-            try(Reader reader = new InputStreamReader(new FileInputStream(file))){
+            try (Reader reader = new InputStreamReader(new FileInputStream(file))) {
                 char[] array = new char[128];
                 int count = reader.read(array);
                 StringBuilder result = new StringBuilder();
                 while (count > 0) {
-                result.append(new String(array, 0, count));
-                count = reader.read(array);
+                    result.append(new String(array, 0, count));
+                    count = reader.read(array);
                 }
                 texts = result.toString().split(" /n");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            for(String a: texts){
+            System.out.println("Text from text files: ");
+            for (String a : texts) {
                 System.out.print(a);
             }
         }
     }
 
-    public static void listOfFiles(String directoryName, List<File> files) {
+    static void listOfFiles(String directoryName, List<File> files) {
         File directory = new File(directoryName);
-        if(directory.exists()) {
+        if (directory.exists()) {
             File[] fList = directory.listFiles();
             if (fList != null) {
                 for (File file : fList) {
@@ -47,6 +112,7 @@ public class Main {
                             getFiles().add(file);
                         }
                     } else if (file.isDirectory()) {
+
                         listOfFiles(file.getAbsolutePath(), getFiles());
                     }
                 }
@@ -59,12 +125,20 @@ public class Main {
             System.out.println("Sorry, wrong name of directory!");
         }
     }
-    public static List<File> getFiles() {
+
+    static List<File> getFiles() {
         return files;
     }
 
-    public static void setFiles(List<File> files) {
+    static void setFiles(List<File> files) {
         Main.files = files;
     }
 
+    static List<File> getTextFiles() {
+        return textFiles;
+    }
+
+    static void setTextFiles(List<File> textFiles) {
+        Main.textFiles = textFiles;
+    }
 }
